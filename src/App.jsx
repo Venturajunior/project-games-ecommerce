390a691e468484598397f15fe7d0e203a7ff6859
clailsonjorge <clailson@notebook_dell.localdomain>
2022-08-13 02:01:32 -0300

    --amend-no-edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import Card from './components/card/Card';
-import Game01 from './assets/Game01.jpg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HeaderBackground from './components/HeaderBackground';
+import './styles/styles.css';
 
 function App() {
   return (
-    <div>
-      <Card
-        img={Game01}
-        gameName="Back for Blood"
-        gameDescription="Back 4 Blood é um jogo de tiro em primeira pessoa dos criadores da franquia aclamada pela crítica Left 4 Dead. Você está no centro de uma guerra contra os contagiados. Esses humanos portadores de um parasita mortal se transformaram em criaturas assustadoras inclinadas a devorar os restos da civilização"
-      />
-    </div>
+    <Router>
+      <div className="container-main">
+        <HeaderBackground />
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>home</h1>} />
+          <Route path="/products" element={<h1>products</h1>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
